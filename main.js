93f5cd348f5de54ec019e174f45b92021cc50890
tech201823 <tech201823@gmail.com>
2018-09-14 15:33:50 +0900

    main.css and javascript fix

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 var rnd = 0;
 var i = 0;
 var level = 6;
@@ -6,6 +8,8 @@ var out = new Array();
 var pic = new Array();
 rnd = Math.floor(Math.random() * 300);
 
+
+
 switch (level) {
   case 6:
     if (rnd < 200) {
@@ -13,24 +17,62 @@ switch (level) {
       hit = Math.floor(Math.random() * 3);
       switch (hit) {
         case 1:
-          document.write('<img src="food.jpg"/>');
-          document.write('<img src="food.jpg"/>');
-          document.write('<img src="food.jpg"/>');
+          document.write('<img src="img/1.jpg"/ width="250" height="250">');
+          document.write('<img src="img/1.jpg"/ width="250" height="250">');
+          document.write('<img src="img/1.jpg"/ width="250" height="250">');
           break;
+
         case 2:
-          document.write('<img src="cat.jpg" />');
-          document.write('<img src="cat.jpg" />');
-          document.write('<img src="cat.jpg" />');
+          document.write('<img src="img/2.jpg"/ width="250" height="250">');
+          document.write('<img src="img/2.jpg"/ width="250" height="250">');
+          document.write('<img src="img/2.jpg"/ width="250" height="250">');
           break;
+
         case 3:
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
+          document.write('<img src="img/3.jpg"/ width="250" height="250">');
+          document.write('<img src="img/3.jpg"/ width="250" height="250">');
+          document.write('<img src="img/3.jpg"/ width="250" height="250">');
           break;
+
+        case 4:
+          document.write('<img src="img/4.jpg"/ width="250" height="250">');
+          document.write('<img src="img/4.jpg"/ width="250" height="250">');
+          document.write('<img src="img/4.jpg"/ width="250" height="250">');
+          break;
+
+        case 5:
+          document.write('<img src="img/5.jpg"/ width="250" height="250">');
+          document.write('<img src="img/5.jpg"/ width="250" height="250">');
+          document.write('<img src="img/5.jpg"/ width="250" height="250">');
+          break;
+
+        case 6:
+          document.write('<img src="img/6.jpg"/ width="250" height="250">');
+          document.write('<img src="img/6.jpg"/ width="250" height="250">');
+          document.write('<img src="img/6.jpg"/ width="250" height="250">');
+          break;
+
+        case 7:
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          break;
+        case 8:
+          document.write('<img src="img/8.jpg"/ width="250" height="250">');
+          document.write('<img src="img/8.jpg"/ width="250" height="250">');
+          document.write('<img src="img/8.jpg"/ width="250" height="250">');
+          break;
+
+        case 9:
+          document.write('<img src="img/9.jpg"/ width="250" height="250">');
+          document.write('<img src="img/9.jpg"/ width="250" height="250">');
+          document.write('<img src="img/9.jpg"/ width="250" height="250">');
+          break;
+
         default:
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
           break;
       }
     } else {
@@ -40,17 +82,23 @@ switch (level) {
       var h = 1;
       //jkhが一緒の時、乱数を入れなおす
       while (j == k && j == h) {
-        j = Math.floor(Math.random() * 2) + 1;
-        k = Math.floor(Math.random() * 2) + 1;
-        h = Math.floor(Math.random() * 2) + 1;
+        j = Math.floor(Math.random() * 8) + 1;
+        k = Math.floor(Math.random() * 8) + 1;
+        h = Math.floor(Math.random() * 8) + 1;
       }
 
-      pic[1] = "school.jpg";
-      pic[2] = "food.jpg";
-      pic[3] = "cat.jpg";
-      document.write('<img src="' + pic[j] + '">');
-      document.write('<img src="' + pic[k] + '">');
-      document.write('<img src="' + pic[h] + '">');
+      pic[1] = "img/1.jpg";
+      pic[2] = "img/2.jpg";
+      pic[3] = "img/3.jpg";
+      pic[4] = "img/4.jpg";
+      pic[5] = "img/5.jpg";
+      pic[6] = "img/6.jpg";
+      pic[7] = "img/7.jpg";
+      pic[8] = "img/8.jpg";
+      pic[9] = "img/9.jpg";
+      document.write('<img src="' + pic[j] + '" width="250" height="250">');
+      document.write('<img src="' + pic[k] + '" width="250" height="250">');
+      document.write('<img src="' + pic[h] + '" width="250" height="250">');
     }
     break;
 
@@ -60,24 +108,62 @@ switch (level) {
       hit = Math.floor(Math.random() * 3);
       switch (hit) {
         case 1:
-          document.write('<img src="food.jpg"/>');
-          document.write('<img src="food.jpg"/>');
-          document.write('<img src="food.jpg"/>');
+          document.write('<img src="img/1.jpg"/ width="250" height="250">');
+          document.write('<img src="img/1.jpg"/ width="250" height="250">');
+          document.write('<img src="img/1.jpg"/ width="250" height="250">');
           break;
+
         case 2:
-          document.write('<img src="cat.jpg" />');
-          document.write('<img src="cat.jpg" />');
-          document.write('<img src="cat.jpg" />');
+          document.write('<img src="img/2.jpg"/ width="250" height="250">');
+          document.write('<img src="img/2.jpg"/ width="250" height="250">');
+          document.write('<img src="img/2.jpg"/ width="250" height="250">');
           break;
+
         case 3:
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
+          document.write('<img src="img/3.jpg"/ width="250" height="250">');
+          document.write('<img src="img/3.jpg"/ width="250" height="250">');
+          document.write('<img src="img/3.jpg"/ width="250" height="250">');
           break;
+
+        case 4:
+          document.write('<img src="img/4.jpg"/ width="250" height="250">');
+          document.write('<img src="img/4.jpg"/ width="250" height="250">');
+          document.write('<img src="img/4.jpg"/ width="250" height="250">');
+          break;
+
+        case 5:
+          document.write('<img src="img/5.jpg"/ width="250" height="250">');
+          document.write('<img src="img/5.jpg"/ width="250" height="250">');
+          document.write('<img src="img/5.jpg"/ width="250" height="250">');
+          break;
+
+        case 6:
+          document.write('<img src="img/6.jpg"/ width="250" height="250">');
+          document.write('<img src="img/6.jpg"/ width="250" height="250">');
+          document.write('<img src="img/6.jpg"/ width="250" height="250">');
+          break;
+
+        case 7:
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          break;
+        case 8:
+          document.write('<img src="img/8.jpg"/ width="250" height="250">');
+          document.write('<img src="img/8.jpg"/ width="250" height="250">');
+          document.write('<img src="img/8.jpg"/ width="250" height="250">');
+          break;
+
+        case 9:
+          document.write('<img src="img/9.jpg"/ width="250" height="250">');
+          document.write('<img src="img/9.jpg"/ width="250" height="250">');
+          document.write('<img src="img/9.jpg"/ width="250" height="250">');
+          break;
+
         default:
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
-          document.write('<img src="school.jpg"/>');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
+          document.write('<img src="img/7.jpg"/ width="250" height="250">');
           break;
       }
     } else {
@@ -91,12 +177,18 @@ switch (level) {
         h = Math.floor(Math.random() * 2) + 1;
       }
 
-      pic[1] = "school.jpg";
-      pic[2] = "food.jpg";
-      pic[3] = "cat.jpg";
-      document.write('<img src="' + pic[j] + '">');
-      document.write('<img src="' + pic[k] + '">');
-      document.write('<img src="' + pic[h] + '">');
+      pic[1] = "img/1.jpg";
+      pic[2] = "img/2.jpg";
+      pic[3] = "img/3.jpg";
+      pic[4] = "img/4.jpg";
+      pic[5] = "img/5.jpg";
+      pic[6] = "img/6.jpg";
+      pic[7] = "img/7.jpg";
+      pic[8] = "img/8.jpg";
+      pic[9] = "img/9.jpg";
+      document.write('<img src="' + pic[j] + '" width="250" height="250">');
+      document.write('<img src="' + pic[k] + '" width="250" height="250">');
+      document.write('<img src="' + pic[h] + '" width="250" height="250">');
       break;
 
 
